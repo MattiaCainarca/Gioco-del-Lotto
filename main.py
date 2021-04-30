bb4d3e70f23c80156b7f4bbb54da614b6d608b9e
MattiaCainarca <mattia.cainarca@gmail.com>
2021-04-30 13:19:16 +0200

    primo

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import codicefiscale
-from datetime import date
+from datetime import datetime
+import numpy as np
+
+nomiRuote = ["Torino", "Milano", "Venezia", "Genova", "Firenze", "Roma", "Napoli", "Bari", "Palermo", "Cagliari", "Ruota Nazionale"]
 
 
-def calcoloEta(giorno, mese, anno):
-    today = date.today()
-    return date.year - anno - ((today.month, today.day) < (mese, giorno))
+def calcolaEta(giorno, mese, anno):
+    today = datetime.today()
+    return today.year - anno - ((today.month, today.day) < (mese, giorno))
 
 
-dataNascita = (codicefiscale.get_birthday('CNRMTT02L16I441L'))
+def scriviFile(mat):
+    now = datetime.now()
+    filename = now.strftime("NumeriVincenti_%m-%d-%Y")
+    filename = f'{filename}.txt'
+    file = open(filename, 'w')
+    np.savetxt(file, mat, fmt='%i', delimiter=';', newline='n', )
+    file.close()
+
+
+dataNascita = (codicefiscale.get_birthday('CNRMTT02L16U441L'))
 dataNascitaArray = (dataNascita.split('-'))
+if int(dataNascitaArray[2]) < 21:
+    dataNascitaArray[2] = int(dataNascitaArray[2]) + 2000
+eta = calcolaEta(int(dataNascitaArray[0]), int(dataNascitaArray[1]), int(dataNascitaArray[2]))
+print(eta)
+
+mat = np.random.randint(1, 91, (11, 5))  # Matrice delle ruote
+print(mat)
+scriviFile(mat)

7130b05199227a2069a0e5bbd94ea192c4812a07
MattiaCainarca <mattia.cainarca@gmail.com>
2021-04-30 13:39:12 +0200

    primo

--- main.py
+++ main.py
@@ -5,27 +5,36 @@ import numpy as np
 nomiRuote = ["Torino", "Milano", "Venezia", "Genova", "Firenze", "Roma", "Napoli", "Bari", "Palermo", "Cagliari", "Ruota Nazionale"]
 
 
+def calcoloCodiceFiscale():
+    codice_fiscale_inserito = -1
+    while not codicefiscale.isvalid(codice_fiscale_inserito):
+        print("\n\nInserisci il tuo codice fiscale: ")
+        codice_fiscale_inserito = input()
+        codice_fiscale_inserito = codice_fiscale_inserito.upper()
+    return codice_fiscale_inserito
+
+
 def calcolaEta(giorno, mese, anno):
     today = datetime.today()
     return today.year - anno - ((today.month, today.day) < (mese, giorno))
 
 
-def scriviFile(mat):
+def scriviFile(matrice):
     now = datetime.now()
     filename = now.strftime("NumeriVincenti_%m-%d-%Y")
     filename = f'{filename}.txt'
     file = open(filename, 'w')
-    np.savetxt(file, mat, fmt='%i', delimiter=';', newline='n', )
+    np.savetxt(file, matrice, fmt='%i', delimiter=';', newline='n', )
     file.close()
 
 
-dataNascita = (codicefiscale.get_birthday('CNRMTT02L16U441L'))
+codice_fiscale = calcoloCodiceFiscale()
+dataNascita = (codicefiscale.get_birthday(codice_fiscale))
 dataNascitaArray = (dataNascita.split('-'))
 if int(dataNascitaArray[2]) < 21:
     dataNascitaArray[2] = int(dataNascitaArray[2]) + 2000
 eta = calcolaEta(int(dataNascitaArray[0]), int(dataNascitaArray[1]), int(dataNascitaArray[2]))
 print(eta)
-
 mat = np.random.randint(1, 91, (11, 5))  # Matrice delle ruote
 print(mat)
 scriviFile(mat)
